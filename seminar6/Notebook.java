fee0585392f6622c255b0d14d22f1da50d7cc265
Alexey <alex3122007@yandex.ru>
2023-06-28 00:13:23 +0300

    homework6.1

--- seminar6/Notebook.java
+++ seminar6/Notebook.java
@@ -7,31 +7,39 @@ public class Notebook {
     private int os;
     private int diag;
 
-public Notebook (int id, int ramsize, int hddsize, int os, int diag){
-    this.id = id;
-    this.ramSize =ramsize;
-    this.hddSize = hddsize;
-    this.os= os;
-    this.diag = diag;
-}
-public int getRamSize() {
-    return ramSize;
-}
-public int getHddSize() {
-    return hddSize;
-}
-public String getOsVer() {
-    String osver = "Windows";
-    if (os == 2){osver = "Linux  ";}
-    return osver;
-}
-public int getDiag() {
-    return diag;
-}
-public int getNum() {
-    return id;
-}
-public int getOsIndex() {
-    return os;
-}
+    public Notebook(int id, int ramsize, int hddsize, int os, int diag) {
+        this.id = id;
+        this.ramSize = ramsize;
+        this.hddSize = hddsize;
+        this.os = os;
+        this.diag = diag;
+    }
+
+    public int getRamSize() {
+        return ramSize;
+    }
+
+    public int getHddSize() {
+        return hddSize;
+    }
+
+    public String getOsVer() {
+        String osver = "Windows";
+        if (os == 2) {
+            osver = "Linux  ";
+        }
+        return osver;
+    }
+
+    public int getDiag() {
+        return diag;
+    }
+
+    public int getNum() {
+        return id;
+    }
+
+    public int getOsIndex() {
+        return os;
+    }
 }
